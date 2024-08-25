b536ee702a036468e5d6052ae1828faacf2ab2c9
Danylo <danillitvin531@gmail.com>
2024-08-25 21:46:13 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import React, { useState } from 'react';
 import './App.css';
 import List from './List';
 
 function App() {
-  const [goodsInCart, setGoodsInCart] = useState(0);
 
   const fruits = [
-    {id: 1, name: "apple", desc: "very fresh and tasty apples"}, 
-    {id: 2, name: "banana", desc: "very fresh and tasty bananas"}, 
-    {id: 3, name: "kiwi", desc: "very fresh and tasty kiwis"}, 
-    {id: 4, name: "pineapple", desc: "very fresh and tasty pineapples"}
+    {id: 1, name: "apple", desc: "very fresh and tasty apples", price: "2$"}, 
+    {id: 2, name: "banana", desc: "very fresh and tasty bananas", price: "2$"}, 
+    {id: 3, name: "kiwi", desc: "very fresh and tasty kiwis", price: "1$"}, 
+    {id: 4, name: "pineapple", desc: "very fresh and tasty pineapples", price: "3$"}
   ];
-
-  const addToCart = () => {
-    setGoodsInCart(goodsInCart + 1);
-  };
-
-  const clearTheCart = () => {
-    setGoodsInCart(0)
-  }
 
   return (
     <>
-      <p>Goods in Cart: {goodsInCart}</p>
-      <button className="clear-button" onClick={clearTheCart}>Clear the Cart</button>
-      <List items={fruits} category="Fruits" onAddToCart={addToCart} />
+      <List items={fruits} category="Fruits" />
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,22 +1,51 @@
 import React, { useState } from "react"
 import './List.css'
-import Button from './Button'
 
-function List({ items, category, onAddToCart }) {
+function List({ items, category }) {
     const [isListVisible, setIsListVisible] = useState(false)
+    const [goodsInCart, setGoodsInCart] = useState([]);
+    const [priceList, setPriceList] = useState([])
 
+    
     const toggleListVisibility = () => {
         setIsListVisible(!isListVisible)
+    }
+
+    const clearTheCart = () => {
+        setGoodsInCart([])
+        setPriceList([])
+    }
+    
+    const handleRemoveGood = (index) => {
+        setGoodsInCart(g => g.filter((_, i) => i !== index));
+        setPriceList(p => p.filter((_, i) => i !== index))
     }
     
     const listItems = items.map(item => (
         <li key={item.id}>
-            <p>Name: {item.name}, <b>description: {item.desc}</b></p>
-            <Button onAddToCart={onAddToCart} />
+            <p>Name: {item.name}, <b>description: {item.desc}</b>. Price: {item.price}</p>
+            <input type="hidden" value={item.name}></input>
+            <button className="add-to-cart-btn" 
+                    onClick={() => {
+                        const priceValue = item.price.split("$");
+                        console.log(priceValue[0]);
+                        setGoodsInCart(g => [...g, item.name]);
+                        setPriceList(p => [...p, Number(priceValue[0])])
+                        }}>
+                <b>Add to Cart</b>
+            </button>
         </li>
     ));
 
-    return (
+    return (<>
+        <div>
+        <p>Goods in Cart: {goodsInCart.length}</p>
+        <p>Total Price: {priceList.reduce((acc, curr) => {return acc + curr}, 0)}$</p>
+        <ul className="goods-list">
+            {goodsInCart.map((good, index) => <li key={index} onClick={() => handleRemoveGood(index)}> {good} </li>)}
+        </ul>
+        <button className="clear-button" onClick={clearTheCart}>Clear the Cart</button>
+        </div>
         <div>
             <h2 className="list-category" onClick={toggleListVisibility}> 
                 {category} 
@@ -27,7 +56,7 @@ function List({ items, category, onAddToCart }) {
                 </ul>
             )}
         </div>
-    )
+    </>)
 }
 
 export default List
